85eb2330d20dee8204a6f6b8353c73c28cac9c43
jorlin roy <jroy46@my.centennialcollege.ca>
2024-12-04 23:18:17 -0500

    updated
    4/12/2024

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ app.use(bodyParser.json());
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
   })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
